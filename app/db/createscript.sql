2f5060e1613d4a0f779ddb74d0fd6b2bd3ead187
Bejan Afkar <336498@student.mboutrecht.nl>
2025-06-27 16:14:35 +0200

    aanpassinge kleur

--- app/db/createscript.sql
+++ app/db/createscript.sql
@@ -317,11 +317,7 @@ CREATE TABLE IF NOT EXISTS `productleverancier` (
   KEY `LeverancierID` (`LeverancierID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
 
---
--- Tabelstructuur voor tabel `rol`
---
 
 DROP TABLE IF EXISTS `rol`;
 CREATE TABLE IF NOT EXISTS `rol` (
